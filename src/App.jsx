75677f453a062d97dd10780577135e74e46cf728
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-08 16:27:08 -0300

    feat(naoEncontrado): Criando página de exibição para rotas não listadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Login from './pages/login'
+import NaoEncontrado from './pages/NaoEncontrado'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='*' element={<NaoEncontrado />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/NaoEncontrado/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, Box, Typography, Button } from '@mui/material';
+import { useNavigate } from "react-router-dom";
+
+const NaoEncontrado = () => {
+    const navigate = useNavigate();
+
+    return (
+        <Container maxWidth={false} sx={{ width: '100vw', height: '100vh' }}>
+            <Box sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <Typography variant="h1">404</Typography>
+                <Typography variant="h5">Página não encontrada</Typography>
+                <Button variant="outlined" onClick={() => { navigate("/") }} sx={{ marginTop: '1rem' }}>Voltar para o Login</Button>
+            </Box>
+        </Container>
+    )
+}
+
+export default NaoEncontrado;
