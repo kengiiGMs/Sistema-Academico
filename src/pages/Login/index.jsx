6d295ebe72525367d368bf7990b975a3d48953aa
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-09 17:29:19 -0300

    refactor(login): Aplicando estruturas reutilizáveis (componentes/hooks)

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,58 +1,57 @@
-import { useState, useEffect } from 'react';
-import { Container, Box, Card, CardContent, Typography, TextField, Button, Stack, CircularProgress, Link } from '@mui/material';
+import { useState } from 'react';
+
 import usarLogin from '../../hooks/login/usarLogin';
-import Cookies from 'js-cookie';
-import { useNavigate } from 'react-router-dom';
-import usarValidarToken from "../../hooks/validarToken/usarValidarToken";
+
+import useRedirecionarSeAutenticado from '../../hooks/autenticado/useRedirecionarSeAutenticado';
+
+import { Typography, TextField, Button, Link } from '@mui/material';
+
+import ContainerCentralizado from '../../components/containerCentralizado';
+import IndicadorDeCarregamento from '../../components/indicadorDeCarregamento';
+import CartaoComFormulario from '../../components/cartaoComFormulario';
 
 const Login = () => {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const token = Cookies.get('token');
-        if (token) {
-            const valido = usarValidarToken(token);
-            if (valido) {
-                navigate('/home');
-            } else {
-                Cookies.remove('token');
-            }
-        }
-    }, [])
-
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
     const [solicitar, loading] = usarLogin();
 
-    const createHandle = async (e) => {
+    useRedirecionarSeAutenticado();
+
+
+
+    const fazerLogin = async (e) => {
         e.preventDefault();
         await solicitar(email, senha);
     }
 
     return (
-        <Container maxWidth={false} sx={{ height: '90vh' }}>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-                {loading ? (
-                    <CircularProgress size="3rem" />
-                ) : (
-                    <Card sx={{ width: { xs: '70vw', sm: '50vw', lg: '30vw' }, p: 1 }}>
-                        <CardContent sx={{ display: 'flex', flexDirection: 'column' }}>
-                            <form onSubmit={createHandle}>
-                                <Stack spacing={{ xs: 1, sm: 2 }}>
-                                    <Typography variant="h5" align='center'>Faça o seu Login!</Typography>
-                                    <TextField id="email" label="Insira o seu Email" variant="filled" onChange={(e) => { setEmail(e.target.value) }} required />
-                                    <TextField id="password" label="Insira a sua Senha" variant="filled" type="password" autoComplete="current-password" onChange={(e) => { setSenha(e.target.value) }} required />
-                                    <Button variant="contained" type="submit" disabled={loading}>Fazer Login</Button>
-                                    <Link href="/cadastrarLogin" underline="hover" textAlign="center">
-                                        Criar Conta
-                                    </Link>
-                                </Stack>
-                            </form>
-                        </CardContent>
-                    </Card>
-                )}
-            </Box>
-        </Container >
+        <ContainerCentralizado>
+            {loading ? (
+                <IndicadorDeCarregamento tamanho="3rem" />
+            ) : (
+                <CartaoComFormulario funcaoAoEnviarFormulario={fazerLogin}>
+                    <Typography variant="h5" align='center'>Faça o seu Login!</Typography>
+                    <TextField
+                        id="email"
+                        label="Insira o seu Email"
+                        variant="filled"
+                        onChange={(e) => { setEmail(e.target.value) }}
+                        required
+                    />
+                    <TextField
+                        id="senha"
+                        label="Insira a sua Senha"
+                        variant="filled"
+                        type="password"
+                        autoComplete="current-password"
+                        onChange={(e) => { setSenha(e.target.value) }}
+                        required
+                    />
+                    <Button variant="contained" type="submit" disabled={loading}>Fazer Login</Button>
+                    <Link href="/cadastrarLogin" underline="hover" textAlign="center">Criar Conta</Link>
+                </CartaoComFormulario>
+            )}
+        </ContainerCentralizado>
     )
 }
 
