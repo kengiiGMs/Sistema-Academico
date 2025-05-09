2bc2083f9c4b50ef7c711a76b7704bc8d94103e4
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-09 11:37:11 -0300

    feat(cadastrarLogin): Atribuindo o hook usarCriar na página cadastrarLogin

--- src/pages/CadastrarLogin/index.jsx
+++ src/pages/CadastrarLogin/index.jsx
@@ -1,37 +1,46 @@
 import { useState } from 'react';
-import { Container, Box, Card, CardContent, Typography, TextField, Button, Stack, Link } from '@mui/material';
-
+import { Container, Box, Card, CardContent, Typography, TextField, Button, Stack, Link, CircularProgress } from '@mui/material';
+import usarCriar from '../../hooks/usuario/usarCriar';
 
 const CadastrarLogin = () => {
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
     const [senhaConfirmar, setSenhaConfirmar] = useState("");
+    const [solicitar, loading] = usarCriar();
 
     const criarUsuario = async (e) => {
         e.preventDefault();
-        alert(`${email} | ${senha} | ${senhaConfirmar}`)
+        if (senha != senhaConfirmar) {
+            alert("As senhas não são iguais");
+        } else {
+            await solicitar(email, senha);
+        }
     }
 
     return (
         <Container maxWidth={false} sx={{ width: '100vw', height: '100vh' }}>
             <Box sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
 
-                <Card sx={{ width: { xs: '70vw', sm: '50vw', lg: '30vw' }, p: 1 }}>
-                    <CardContent sx={{ display: 'flex', flexDirection: 'column' }}>
-                        <form onSubmit={criarUsuario}>
-                            <Stack spacing={{ xs: 1, sm: 2 }}>
-                                <Typography variant="h5" align='center'>Cadastre o seu Login!</Typography>
-                                <TextField id="email" label="Insira o seu Email" variant="filled" onChange={(e) => { setEmail(e.target.value) }} required />
-                                <TextField id="password" label="Insira a sua Senha" variant="filled" type="password" autoComplete="current-password" onChange={(e) => { setSenha(e.target.value) }} required />
-                                <TextField id="passwordConfirmar" label="Confirme a sua Senha" variant="filled" type="password" autoComplete="current-password" onChange={(e) => { setSenhaConfirmar(e.target.value) }} required />
-                                <Button variant="contained" type="submit">Fazer Login</Button>
-                                <Link href="/" underline="hover" textAlign="center">
-                                    Voltar
-                                </Link>
-                            </Stack>
-                        </form>
-                    </CardContent>
-                </Card>
+                {loading ? (
+                    <CircularProgress />
+                ) : (
+                    <Card sx={{ width: { xs: '70vw', sm: '50vw', lg: '30vw' }, p: 1 }}>
+                        <CardContent sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <form onSubmit={criarUsuario}>
+                                <Stack spacing={{ xs: 1, sm: 2 }}>
+                                    <Typography variant="h5" align='center'>Cadastre o seu Login!</Typography>
+                                    <TextField id="email" label="Insira o seu Email" variant="filled" onChange={(e) => { setEmail(e.target.value) }} required />
+                                    <TextField id="password" label="Insira a sua Senha" variant="filled" type="password" autoComplete="current-password" onChange={(e) => { setSenha(e.target.value) }} required />
+                                    <TextField id="passwordConfirmar" label="Confirme a sua Senha" variant="filled" type="password" autoComplete="current-password" onChange={(e) => { setSenhaConfirmar(e.target.value) }} required />
+                                    <Button variant="contained" type="submit">Fazer Login</Button>
+                                    <Link href="/" underline="hover" textAlign="center">
+                                        Voltar
+                                    </Link>
+                                </Stack>
+                            </form>
+                        </CardContent>
+                    </Card>
+                )}
             </Box>
         </Container>
     )
