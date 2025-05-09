d10678384038b53b7ab02605cf22e87123390db5
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-09 20:36:38 -0300

    refactor(home): Aplicando estruturas reutilizáveis (componentes)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,257 +1,113 @@
 import { useEffect, useState } from 'react';
-import { Container, Box, Typography, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Button, CircularProgress, Modal, TextField, Link, AppBar, Toolbar } from '@mui/material';
+import { Container, Box, TableRow, TableCell, Button, } from '@mui/material';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
 import usarListar from '../../hooks/estudante/usarListar';
-import usarCriar from '../../hooks/estudante/usarCriar';
-import usarDeletar from '../../hooks/estudante/usarDeletar';
-import usarDeletarToken from '../../hooks/deletarToken/usarDeletarToken';
-import usarAtualizar from '../../hooks/estudante/usarAtualizar';
+
+import BarraDeNavegacao from '../../components/barraDeNavegacao';
+import IndicadorDeCarregamento from '../../components/indicadorDeCarregamento';
+import Tabela from '../../components/tabela';
+
+import JanelaModalCadastrar from '../../components/janelaModalPersonalizada/janelaModalCriar';
+import JanelaModalDeletar from '../../components/janelaModalPersonalizada/janelaModalDeletar';
+import JanelaModalEditar from '../../components/janelaModalPersonalizada/janelaModalEditar';
 
 const Home = () => {
     const [data, solicitar, loading] = usarListar();
-    const [solicitarCriar, loadingCriar] = usarCriar();
-    const [solicitarDeletar, loadingDeletar] = usarDeletar();
-    const [solicitarDeletarToken] = usarDeletarToken();
-    const [solicitarAtualizar, loadingAtualizar] = usarAtualizar();
 
+    const [abrirModalCadastro, setAbriModalCadastro] = useState(false);
 
-    const [modalCadastro, setModalCadastro] = useState(false);
-    const [nome, setNome] = useState("");
-    const [endereco, setEndereco] = useState("");
-    const [curso, setCurso] = useState("");
-    const [telefone, setTelefone] = useState("");
-    const [email, setEmail] = useState("");
+    const [abrirModalDeletar, setAbriModalDeletar] = useState(false);
+    const [idDeletar, setIdDeletar] = useState('');
+    const [nomeDeletar, setNomeDeletar] = useState('');
 
-    const [modalDeletar, setModalDeletar] = useState(false);
-    const [idDeletar, setIdDeletar] = useState("");
-    const [nomeDeletar, setNomeDeletar] = useState("");
+    const [abrirModalEditar, setAbrirModalEditar] = useState(false);
+    const [idEditar, setIdEditar] = useState('');
+    const [nomeEditar, setNomeEditar] = useState('');
+    const [enderecoEditar, setEnderecoEditar] = useState('');
+    const [cursoEditar, setCursoEditar] = useState('');
+    const [telefoneEditar, setTelefoneEditar] = useState('');
+    const [emailEditar, setEmailEditar] = useState('');
 
-    const [modalAtualizar, setModalAtualizar] = useState(false);
-    const [idAtualizar, setIdAtualizar] = useState("");
-    const [nomeAtualizar, setNomeAtualizar] = useState("");
-    const [enderecoAtualizar, setEnderecoAtualizar] = useState("");
-    const [cursoAtualizar, setCursoAtualizar] = useState("");
-    const [telefoneAtualizar, setTelefoneAtualizar] = useState("");
-    const [emailAtualizar, setEmailAtualizar] = useState("");
-
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-        textAlign: 'center',
-        color: 'black'
-    };
-
-
-    const abrirModalCadastro = () => {
-        setModalCadastro(true);
+    const colunas = ['Nome', 'Telefone', 'Endereco', 'Curso', 'Email', 'Ações'];
+    const cabecalhoDaTabela = () => {
+        return colunas.map((coluna) => {
+            return <TableCell key={coluna}>{coluna}</TableCell>
+        })
     }
 
-    const fecharModalCadastro = () => {
-        setModalCadastro(false);
-    }
-
-    const efetuarCadastro = async (e) => {
-        e.preventDefault();
-        await solicitarCriar(nome, endereco, curso, telefone, email);
-        solicitar();
-        fecharModalCadastro();
-    }
-
-    const abrirModalDeletar = () => {
-        setModalDeletar(true);
-    }
-
-    const fecharModalDeletar = () => {
-        setModalDeletar(false);
+    const corpoDaTabela = () => {
+        return data.map((estudante) => (
+            <TableRow
+                key={estudante.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+                <TableCell component='th' scope='row'>
+                    {estudante.nome}
+                </TableCell>
+                <TableCell align='right'>{estudante.telefone}</TableCell>
+                <TableCell align='right'>{estudante.endereco}</TableCell>
+                <TableCell align='right'>{estudante.curso}</TableCell>
+                <TableCell align='right'>{estudante.email}</TableCell>
+                <TableCell align='right'>
+                    <Button variant='contained' sx={{ marginRight: '10px' }} onClick={() => { prepararParaEditar(estudante.id, estudante.nome, estudante.telefone, estudante.endereco, estudante.curso, estudante.email) }}><EditIcon /></Button>
+                    <Button variant='contained' color='error' onClick={() => { prepararParaDeletar(estudante.id, estudante.nome) }}><DeleteIcon /></Button>
+                </TableCell>
+            </TableRow>
+        ))
     }
 
     const prepararParaDeletar = (id, nome) => {
         setIdDeletar(id);
         setNomeDeletar(nome);
-        abrirModalDeletar();
+        setAbriModalDeletar(true);
     }
 
-    const efetuarExclusao = async (e) => {
-        e.preventDefault();
-
-        await solicitarDeletar(idDeletar);
-        solicitar();
-        fecharModalDeletar();
+    const prepararParaEditar = (id, nome, telefone, endereco, curso, email) => {
+        setIdEditar(id);
+        setNomeEditar(nome);
+        setTelefoneEditar(telefone);
+        setEnderecoEditar(endereco);
+        setCursoEditar(curso);
+        setEmailEditar(email)
+        setAbrirModalEditar(true);
     }
-
-    const abrirModalAtualizar = () => {
-        setModalAtualizar(true);
-    }
-
-    const fecharModalAtualizar = () => {
-        setModalAtualizar(false);
-    }
-
-    const prepararParaAtualizar = (id, nome, telefone, endereco, curso, email) => {
-        setIdAtualizar(id);
-        setNomeAtualizar(nome);
-        setTelefoneAtualizar(telefone);
-        setEnderecoAtualizar(endereco);
-        setCursoAtualizar(curso);
-        setEmailAtualizar(email)
-        abrirModalAtualizar();
-    }
-
-    const efetuarEdicao = async (e) => {
-        e.preventDefault();
-
-        await solicitarAtualizar(nomeAtualizar, enderecoAtualizar, cursoAtualizar, telefoneAtualizar, emailAtualizar, idAtualizar);
-        solicitar();
-    }
-
 
     useEffect(() => {
         solicitar();
     }, []);
 
+    useEffect(() => {
+        if (!abrirModalCadastro, !abrirModalEditar, !abrirModalDeletar, !abrirModalEditar) {
+            solicitar();
+        }
+    }, [abrirModalCadastro, abrirModalEditar, abrirModalDeletar, abrirModalEditar])
+
     return (
         <>
-            <AppBar position="static">
-                <Toolbar>
-
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Sistema Acadêmico
-                    </Typography>
-
-                    <div>
-                        <Button variant="contained" sx={{ marginBlock: '5px' }} disabled={loadingCriar} color='error' onClick={() => { solicitarDeletarToken() }}>Sair</Button>
-                    </div>
-
-                </Toolbar>
-            </AppBar>
+            <BarraDeNavegacao />
 
             <Container maxWidth={false}>
 
                 <Box sx={{ width: '90%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
                     {loading ? (
-                        <CircularProgress size="3rem" />
+                        <IndicadorDeCarregamento tamanho='3rem' />
                     ) : (
                         <Box>
-                            <Typography variant="h1" sx={{ fontSize: '60px', textAlign: 'center' }}>Estudantes</Typography>
 
-                            <Button variant="contained" sx={{ marginBottom: '10px' }} onClick={abrirModalCadastro}>Cadastrar Estudante</Button>
+                            <Button variant='contained' sx={{ marginBottom: '10px', marginTop: '10px' }} onClick={() => { setAbriModalCadastro(true) }}>Cadastrar Estudante</Button>
 
-                            <TableContainer component={Paper} sx={{ maxWidth: 900 }}>
-                                <Table aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>Nome</TableCell>
-                                            <TableCell align="right">Telefone</TableCell>
-                                            <TableCell align="right">Endereço</TableCell>
-                                            <TableCell align="right">Curso</TableCell>
-                                            <TableCell align="right">Email</TableCell>
-                                            <TableCell align="right">Ações</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {data.map((estudante) => (
-                                            <TableRow
-                                                key={estudante.id}
-                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                            >
-                                                <TableCell component="th" scope="row">
-                                                    {estudante.nome}
-                                                </TableCell>
-                                                <TableCell align="right">{estudante.telefone}</TableCell>
-                                                <TableCell align="right">{estudante.endereco}</TableCell>
-                                                <TableCell align="right">{estudante.curso}</TableCell>
-                                                <TableCell align="right">{estudante.email}</TableCell>
-                                                <TableCell align="right">
-                                                    <Button variant="contained" sx={{ marginRight: '10px' }} onClick={() => { prepararParaAtualizar(estudante.id, estudante.nome, estudante.telefone, estudante.endereco, estudante.curso, estudante.email) }}>Atualizar</Button>
-                                                    <Button variant="contained" color='error' onClick={() => { prepararParaDeletar(estudante.id, estudante.nome) }}>Deletar </Button>
-                                                </TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
+                            <Tabela cabecalho={cabecalhoDaTabela()} corpo={corpoDaTabela()} />
 
-                            <Modal
-                                open={modalCadastro}
-                                onClose={fecharModalCadastro}
-                                aria-labelledby="modal-modal-title"
-                                aria-describedby="modal-modal-description"
-                            >
-                                <Box sx={style}>
-                                    <form onSubmit={efetuarCadastro}>
-                                        <Container sx={{ display: 'flex', flexDirection: 'column' }}>
-                                            <Typography variant="h5" sx={{ marginBlock: '5px' }}>Cadastro de Estudante</Typography>
-                                            <TextField id="nome" label="Insira o seu Nome" variant="filled" onChange={(e) => { setNome(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-                                            <TextField id="endereco" label="Insira o seu Endereço" variant="filled" onChange={(e) => { setEndereco(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-                                            <TextField id="curso" label="Insira o seu Curso" variant="filled" onChange={(e) => { setCurso(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-                                            <TextField id="telefone" label="Insira o seu Telefone" variant="filled" onChange={(e) => { setTelefone(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-                                            <TextField id="email" label="Insira o seu Email" variant="filled" onChange={(e) => { setEmail(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-
-                                            <Button variant="contained" type="submit" sx={{ marginBlock: '5px' }} disabled={loadingCriar}>Cadastrar</Button>
-                                        </Container>
-                                    </form>
-                                    <Container>
-                                        <Link sx={{ color: 'red' }} underline='none' onClick={() => { fecharModalCadastro() }}>Cancelar</Link>
-                                    </Container>
-                                </Box>
-                            </Modal>
-
-                            <Modal
-                                open={modalDeletar}
-                                onClose={fecharModalDeletar}
-                                aria-labelledby="modal-modal-title"
-                                aria-describedby="modal-modal-description"
-                            >
-                                <Box sx={style}>
-                                    <form onSubmit={efetuarExclusao}>
-                                        <Container sx={{ display: 'flex', flexDirection: 'column' }}>
-                                            <Typography variant="h5" sx={{ marginBlock: '5px' }}>Deletar Estudante</Typography>
-                                            <Typography variant="h5" sx={{ marginBlock: '25px', marginTop: '10px' }}>Deseja deletar {nomeDeletar} ?</Typography>
-
-
-                                            <Button variant="contained" type="submit" sx={{ marginBlock: '5px' }} disabled={loadingDeletar}>Deletar</Button>
-                                        </Container>
-                                    </form>
-                                    <Container>
-                                        <Link sx={{ color: 'red' }} underline='none' onClick={() => { fecharModalDeletar() }}>Cancelar</Link>
-                                    </Container>
-                                </Box>
-                            </Modal>
-
-                            <Modal
-                                open={modalAtualizar}
-                                onClose={fecharModalAtualizar}
-                                aria-labelledby="modal-modal-title"
-                                aria-describedby="modal-modal-description"
-                            >
-                                <Box sx={style}>
-                                    <form onSubmit={efetuarEdicao}>
-                                        <Container sx={{ display: 'flex', flexDirection: 'column' }}>
-                                            <Typography variant="h5" sx={{ marginBlock: '5px' }}>Atualização de Estudante</Typography>
-                                            <TextField id="nome" label="Insira o seu Nome" variant="filled" value={nomeAtualizar} onChange={(e) => { setNomeAtualizar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-                                            <TextField id="endereco" label="Insira o seu Endereço" variant="filled" value={enderecoAtualizar} onChange={(e) => { setEnderecoAtualizar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-                                            <TextField id="curso" label="Insira o seu Curso" variant="filled" value={cursoAtualizar} onChange={(e) => { setCursoAtualizar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-                                            <TextField id="telefone" label="Insira o seu Telefone" variant="filled" value={telefoneAtualizar} onChange={(e) => { setTelefoneAtualizar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-                                            <TextField id="email" label="Insira o seu Email" variant="filled" value={emailAtualizar} onChange={(e) => { setEmailAtualizar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-
-                                            <Button variant="contained" type="submit" sx={{ marginBlock: '5px' }} disabled={loadingAtualizar}>Atualizar</Button>
-                                        </Container>
-                                    </form>
-                                    <Container>
-                                        <Link sx={{ color: 'red' }} underline='none' onClick={() => { fecharModalAtualizar() }}>Cancelar</Link>
-                                    </Container>
-                                </Box>
-                            </Modal>
+                            <JanelaModalCadastrar abrir={abrirModalCadastro} fechar={() => setAbriModalCadastro(false)} />
+                            <JanelaModalDeletar abrir={abrirModalDeletar} fechar={() => setAbriModalDeletar(false)} nome={nomeDeletar} id={idDeletar} />
+                            <JanelaModalEditar abrir={abrirModalEditar} fechar={() => setAbrirModalEditar(false)} nome={nomeEditar} id={idEditar} endereco={enderecoEditar} curso={cursoEditar} telefone={telefoneEditar} email={emailEditar} />
                         </Box>
                     )}
                 </Box>
-            </Container >
+            </Container>
         </>
     )
 }
