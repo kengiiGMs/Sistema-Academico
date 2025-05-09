fe92861f6c457d93bc87addac2b921921b2b3db4
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-09 18:09:39 -0300

    refactor(naoEcontrado): Aplicando estruturas reutilizáveis (componentes)

--- src/pages/NaoEncontrado/index.jsx
+++ src/pages/NaoEncontrado/index.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Container, Box, Typography, Button } from '@mui/material';
+import { Container, Typography, Button } from '@mui/material';
 import { useNavigate } from "react-router-dom";
+import ContainerCenter from "../../components/containerCentralizado";
 
 const NaoEncontrado = () => {
     const navigate = useNavigate();
 
     return (
-        <Container maxWidth={false} sx={{ height: '90vh' }}>
-            <Box sx={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+        <ContainerCenter>
+            <Container sx={{ width: { xs: '70vw', sm: '50vw', lg: '30vw' }, p: 1, color: 'white', textAlign: 'center' }}>
                 <Typography variant="h1">404</Typography>
                 <Typography variant="h5">Página não encontrada</Typography>
-                <Button variant="outlined" onClick={() => { navigate("/") }} sx={{ marginTop: '1rem' }}>Voltar para o Login</Button>
-            </Box>
-        </Container >
+                <Button variant="outlined" onClick={() => { navigate("/") }} sx={{ marginTop: '1.5vw' }}>Voltar para o Login</Button>
+            </Container>
+        </ContainerCenter>
     )
 }
 
