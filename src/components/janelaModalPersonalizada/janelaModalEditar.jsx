80409344f308e8c643b51b1f363cdaa9b57a6f12
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-10 22:19:35 -0300

    refactor(janelaModalPersonalizada): Corrigindo texto dos input da janelaModalEditar

--- src/components/janelaModalPersonalizada/janelaModalEditar.jsx
+++ src/components/janelaModalPersonalizada/janelaModalEditar.jsx
@@ -28,11 +28,11 @@ const JanelaModalEditar = ({ abrir, fechar, nome, id, endereco, curso, telefone,
     return (
         <JanelaModal fechar={fechar} abrir={abrir} funcaoAoEnviarFormulario={efetuarEdicao} carregando={carregando} textoBotao='Editar' >
             <Typography variant='h5' sx={{ marginBlock: '5px' }}>Atualização de Estudante</Typography>
-            <TextField id='nome' label='Insira o seu Nome' variant='filled' value={nomeEditar} onChange={(e) => { setNomeEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-            <TextField id='endereco' label='Insira o seu Endereço' variant='filled' value={enderecoEditar} onChange={(e) => { setEnderecoEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-            <TextField id='curso' label='Insira o seu Curso' variant='filled' value={cursoEditar} onChange={(e) => { setCursoEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-            <TextField id='telefone' label='Insira o seu Telefone' variant='filled' value={telefoneEditar} onChange={(e) => { setTelefoneEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
-            <TextField id='email' label='Insira o seu Email' variant='filled' value={emailEditar} onChange={(e) => { setEmailEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
+            <TextField id='nome' label='Insira o Nome' variant='filled' value={nomeEditar} onChange={(e) => { setNomeEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
+            <TextField id='endereco' label='Insira o Endereço' variant='filled' value={enderecoEditar} onChange={(e) => { setEnderecoEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
+            <TextField id='curso' label='Insira o Curso' variant='filled' value={cursoEditar} onChange={(e) => { setCursoEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
+            <TextField id='telefone' label='Insira o Telefone' variant='filled' value={telefoneEditar} onChange={(e) => { setTelefoneEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
+            <TextField id='email' label='Insira o Email' variant='filled' value={emailEditar} onChange={(e) => { setEmailEditar(e.target.value) }} required sx={{ marginBlock: '5px' }} />
         </JanelaModal>
     )
 }
