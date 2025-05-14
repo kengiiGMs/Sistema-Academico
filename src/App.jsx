9650b8cdc299cdb38ee04a0820b9bb21bc98b732
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-14 12:34:33 -0300

    feat(estudante/hooks): Adicionando página e hook para cadastrar estudante

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import NaoEncontrado from './pages/NaoEncontrado'
 import CadastrarLogin from './pages/CadastrarLogin'
+import CadastrarLoginEstudante from './pages/CadastrarLoginEstudante'
 import Gestao from './pages/Gestao'
 
 import RotaProtegidaDiretor from './middlewares/RotaProtegidaDiretor'
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/cadastrarLogin' element={<CadastrarLogin />} />
+        <Route path='/cadastrarLogin/estudante' element={<CadastrarLoginEstudante />} />
         <Route path='/gestao' element={<RotaProtegidaDiretor><Gestao /></RotaProtegidaDiretor>} />
         <Route path='*' element={<NaoEncontrado />} />
       </Routes>
